41f6b95fcade58bdc5d395cc4aae03e4436f0ec0
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2023-11-29 14:15:51 +0100

    Added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom";
-
-import Homepage from './views/Homepage';
-import About from './views/About';
-import Error from './views/Error';
 
 function App() {
 
   return (
-    <Routes>
-      <Route path='/branch-example' element={ Homepage } />
-      <Route path='/om' element={ About } />
-      <Route path='/*' element={ Error } />
-    </Routes>
+    <div className="App">
+      <h1>Detta är startsidan.</h1>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { 
+  RouterProvider 
+} from 'react-router-dom';
 import './index.css';
-
-import App from './App';
 
 import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={ router } />
   </React.StrictMode>,
 )
